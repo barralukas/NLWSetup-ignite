17a9edbd4d5abd68037ef8aed6aa3c39d4525cd2
Lucas <lucasna256@gmail.com>
2023-01-17 11:16:19 -0300

    criando setup backend

---
+++ server/src/server.ts
@@ -0,0 +1,20 @@
+import Fastify from "fastify";
+import cors from "@fastify/cors";
+import { PrismaClient } from '@prisma/client'
+
+const app = Fastify()
+//Conexão para ter acesso as tabelas no banco de dados
+const prisma = new PrismaClient()
+
+app.register(cors)
+
+app.get('/', async () => {
+    const habits = await prisma.habit.findMany()
+    return habits
+})
+
+app.listen({
+    port: 3333,
+}).then(() => {
+    console.log('HTTP Server running!');
+})
